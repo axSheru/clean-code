0d14a891a2940c0eafa2b4be2121be3acc467c92
Alex Paredes <alexparedesr95@gmail.com>
2022-09-05 12:11:38 -0500

    FEAT: SOLID - Added examples of open close principle.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 // import './clean-code/07-tarea-resuelta';
-import './code-smells/02-low-coupling';
+import './solid/02-open-close-a';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
