2606f574d249e137effec93dedc81f29456cb8fe
Alex Paredes <alexparedesr95@gmail.com>
2022-09-05 12:32:00 -0500

    REFACTOR: SOLID - Applied open closed principle with axios.

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,10 +1,18 @@
-import axios from "axios";
+// import axios from "axios";
 
 export class HttpClient {
 
-    async get( url: string ) {
+    /* async get( url: string ) {
         const { data, status } = await axios.get( url );
         return { data, status };
+    } */
+
+    async get( url: string ) {
+
+        const resp = await fetch( url );
+        const data = await resp.json();
+
+        return { data, status: resp.status };
     }
 
 }
